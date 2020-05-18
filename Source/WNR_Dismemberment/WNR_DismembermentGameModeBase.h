c07d822e82f8e6245ba72481971643ae3652bc32
Nicholas Joseph <NfrancisJ@gmail.com>
2020-05-18 15:16:12 -0700

    no message

---
+++ Source/WNR_Dismemberment/WNR_DismembermentGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "WNR_DismembermentGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WNR_DISMEMBERMENT_API AWNR_DismembermentGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
